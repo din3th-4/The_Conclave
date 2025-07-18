99ed4e785e2b9f3a30fd7518ba62051532a21851
din3th-4 <sheharadineth14@gmail.com>
2025-07-18 17:48:34 +0200

    Initial commit

---
+++ src/cardinal/Cardinal.java
@@ -0,0 +1,42 @@
+package cardinal;
+
+import java.util.LinkedHashMap;
+
+public class Cardinal implements Runnable {
+
+    private String name ;
+    private LinkedHashMap profile ;
+    private LinkedHashMap whoIVote ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void run() {
+
+    }
+
+
+    public String getName() {
+        return name ;
+    }
+    public LinkedHashMap getProfile() {
+        return profile ;
+    }
+
+
+}
---
+++ src/cardinal/Characters.java
@@ -0,0 +1,57 @@
+package cardinal;
+
+class Characters {
+    private Characters() {
+    }
+
+    static final String[] characteristics = {"continent", "age", "charisma", "popularity", "deception", "politicalCons",
+            "religiousCons", "societalViews", "econIdeology", "ambition", "churchAuth",
+            "localPower", "multilingual", "health", "loyalty", "voteSelf", "rigidness"} ;
+
+    static String[] continents = {"Asia", "Europe", "Africa", "NAmerica", "SAmerica", "Australia"} ;
+
+    static String[] Europe = {"Vincent Rinaldi", "Luca Bellini", "Krzysztof Wozniak", "Henrik Faulkner",
+            "Jean-Marc Valois", "Andrei Petrescu", "Eduard Schreiber", "Fernando Alvito", "Seamus Donnelly", "Janos Kovács",
+            "Bastien Moreau", "Otto Neumann", "Marco Vespucci", "Tomasz Lisowski", "Armand Lefevre",
+            "Nikolai Dragunov", "Sven Dahlberg", "Giuseppe Romano", "Rafael Muñoz", "Willem van Dijk",
+            "Bogdan Ionescu", "Artur Malinowski", "Dimitri Volkov", "Claude Perrin", "Rudolf Schneider"
+    };
+
+    static String[]  NAmerica = {"Michael Donovan", "Raymond Lancaster", "Charles Wentworth", "Samuel O’Brien",
+            "Thomas Galloway", "Isaac Montgomery", "Jacob Whitman", "Christopher Nash", "Robert Holloway", "Nathaniel Brooks",
+            "Edward Prescott", "Anthony Harper", "Brian Callahan", "Gregory Monroe", "Dominic Carver",
+            "Elijah Shepherd", "Caleb Thornton", "Lucas McAllister", "Owen Fitzgerald", "Gavin Marshall",
+            "Patrick Delaney", "Walter Blackwell", "Benjamin Caldwell", "Ethan Granger", "Logan Whitaker"
+    };
+
+    static String[] SAmerica = {"Francisco Almeida", "Julio Peralta", "Andrés Salgado", "Domingo Herrera",
+            "Esteban Torres", "Carlos Menéndez", "Ignacio Morales", "Rafael Barreto", "Pedro Alvarado", "Luciano Rivas",
+            "Mateo Berríos", "Alonso Ferreira", "Santiago Moya", "Marcelo Tavares", "Diego Pacheco",
+            "Jorge Nascimento", "Arturo Ledesma", "Felipe Montenegro", "Enrique Caballero", "Simón Duarte",
+            "Camilo Araujo", "Luis Arancibia", "Manuel Carranza", "Victor Espinoza", "Rodrigo Medina"
+    };
+
+    static String[] Africa = { "Joseph N'Kosi", "Gabriel Mensah", "Samuel Okonkwo", "Benoit Lumumba", "Peter Mbare",
+            "Abel Diouf", "John Chikere", "Emmanuel Sefu", "David Madu", "Ahmed Keita",
+            "Tariq Mahama", "Paul Muwanga", "Isaac Tshabalala", "Benjamin Eze", "Ezekiel Bongo",
+            "Theophilus Kunda", "Malik Adeyemi", "Caleb Abiola", "Simon Mugabe", "Nathan Obi",
+            "Victor Makoni", "Elijah Barongo", "Thomas Kouadio", "James Kibet", "Anthony Mawere"
+    };
+
+
+    static String[] Asia = {"Kenji Sakamoto", "Ravi Fernandez", "Bao Xun-Li", "Miguel Arantes", "Anil Chowdhury",
+            "Takeshi Nakamura", "Haruto Fujimoto", "Sang-Woo Kim", "Phuc Le", "Tenzin Dorji",
+            "Farid Hasan", "Daisuke Mori", "Imran Siddiqui", "Surya Rajan", "Chen Guang-Yu",
+            "Ali Bashir", "Shigeru Okada", "Yusuf Al-Rashid", "Nguyen Van Minh", "Rajesh Kapoor",
+            "Jamal Arif", "Mohammed Tanveer", "Junichi Takeda", "Amir Ghaffari", "Tariq Noor"
+    };
+
+    static String[] Australia = { "Matthew Reilly", "Noah Te Atarangi", "Daniel MacLeod", "James Kalani",
+            "Lucas Harrington", "Blake Rutherford", "Callum O'Connor", "Kauri Henare", "Julian West", "Isaac Dunbar",
+            "Hamish Fraser", "Angus Boyd", "Liam Te Paea", "Tyson Redfern", "Nathan Kealoha",
+            "Jordan Withers", "Cooper Wainui", "Kai Hekili", "Dylan McGregor", "Lachlan Kiri",
+            "Owen Ngata", "Patrick Rua", "Sean Makai", "Jacob Anaru", "Xavier Raukawa"
+    };
+
+
+}
